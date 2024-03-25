112fdc9c407c039c3c7a56ce43759ac2ab5d1600
Gaaaai <107447708+Gaaaai@users.noreply.github.com>
2024-03-25 19:00:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,10 +22,8 @@ const USER_CONFIG = {
       name: '改改',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o4hSe6NkLjh8q2ycwImp7eE2KoCQ',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o4hSe6Ha8pqXZr_QIj8IFDw1BStU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	JFgboT4jZ59G9NrnRalBeOVLM7Wn5UorB6djM1vKr3Y',
+      useTemplateId: 'JFgboT4jZ59G9NrnRalBeOVLM7Wn5UorB6djM1vKr3Y',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-06',
       festivals: [
